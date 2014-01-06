f99eefc7bcf6035b0f17257a32c4fb8b961b7078
Jonathan Bither <jonbither@gmail.com>
2014-01-06 10:32:53 -0500

    SELinux: Set permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ BOARD_WANTS_EMMC_BOOT := true
 # Kernel
 BOARD_KERNEL_BASE := 0x48800000
 BOARD_KERNEL_PAGE_SIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL3 androidboot.hardware=vigor no_console_suspend=1
+BOARD_KERNEL_CMDLINE := console=ttyHSL3 androidboot.hardware=vigor no_console_suspend=1 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 TARGET_KERNEL_VERSION := 3.0
 TARGET_KERNEL_CONFIG := vigor_aosp_defconfig
