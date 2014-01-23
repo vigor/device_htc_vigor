d474390f7eaf11ba2210f803043058b33e520581
Snuzzo <jrracinfan@gmail.com>
2014-01-23 14:10:23 -0500

    Temp commit: Full overlay

--- ill.mk
+++ ill.mk
@@ -5,7 +5,6 @@ $(call inherit-product, vendor/illusion/config/cdma.mk)
 
 $(call inherit-product, device/htc/vigor/vigor.mk)
 
-DEVICE_PACKAGE_OVERLAYS := device/htc/vigor/illusion-overlay
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
